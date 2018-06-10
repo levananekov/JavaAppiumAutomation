7ca9cbd28b119f18441e3cdd032fd29247cd09ca
steep_11 <steep_1183@gmail.com>
2018-06-10 15:51:28 +0500

    Ex5:

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -6,10 +6,7 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.ScreenOrientation;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -524,6 +521,160 @@ public class FirstTest {
                 By.xpath("//*[@resource-id = 'org.wikipedia:id/page_list_item_container']//*[@text='Object-oriented programming language']"),
                 "Cannot find article after returning from background.",
                 5
+        );
+    }
+
+    @Test
+    public void saveSecondArticleToMyList()
+    {
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+                "Cannot find 'Поиск по Википедии' input.",
+                5
+        );
+
+        waitForElementAndSendKeys(
+                By.xpath("//*[@text='Search…']"),
+                "Java",
+                "Cannot find search input.",
+                5
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[@resource-id = 'org.wikipedia:id/page_list_item_container']//*[@text='Object-oriented programming language']"),
+                "Cannot find 'Object-oriented programming language' input.",
+                5
+        );
+
+        waitForElementPresent(
+                By.id("org.wikipedia:id/view_page_title_text"),
+                "Cannot find ArticleTitle",
+                15
+        );
+
+        waitForElementAndClick(
+                By.xpath("//android.widget.ImageView[@content-desc= 'More options']"),
+                "Cannot find button to open article options",
+                5
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[@text= 'Add to reading list']"),
+                "Cannot find options to add article to reading list ",
+                5
+        );
+
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/onboarding_button"),
+                "Cannot find 'Got it' tip overlay",
+                5
+        );
+
+        waitForElementAndClear(
+                By.id("org.wikipedia:id/text_input"),
+                "Cannot find input to set articles folder",
+                5
+        );
+
+        String name_of_folder = "Learning programming";
+
+        waitForElementAndSendKeys(
+                By.id("org.wikipedia:id/text_input"),
+                name_of_folder,
+                "Cannot put text into articles folder input",
+                5
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[@text= 'OK']"),
+                "Cannot press 'ok' button",
+                5
+        );
+
+        waitForElementAndClick(
+                By.xpath("//android.widget.ImageButton[@content-desc= 'Navigate up']"),
+                "Cannot close article, cannot find 'X' link.",
+                5
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+                "Cannot find 'Поиск по Википедии' input.",
+                5
+        );
+
+        waitForElementAndSendKeys(
+                By.xpath("//*[@text='Search…']"),
+                "Python",
+                "Cannot find search input.",
+                5
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[@resource-id = 'org.wikipedia:id/page_list_item_container']//*[@text='General-purpose, high-level programming language']"),
+                "Cannot find 'General-purpose, high-level programming language' input.",
+                5
+        );
+
+        waitForElementPresent(
+                By.id("org.wikipedia:id/view_page_title_text"),
+                "Cannot find 'Python' ArticleTitle.",
+                15
+        );
+
+        waitForElementAndClick(
+                By.xpath("//android.widget.ImageView[@content-desc= 'More options']"),
+                "Cannot find button to open article options",
+                5
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[@text= 'Add to reading list']"),
+                "Cannot find options to add article to reading list ",
+                5
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[@resource-id = 'org.wikipedia:id/list_of_lists']//*[@text='"+ name_of_folder +"']"),
+                "Cannot find 'General-purpose, high-level programming language' input.",
+                5
+        );
+
+        waitForElementAndClick(
+                By.xpath("//android.widget.ImageButton[@content-desc= 'Navigate up']"),
+                "Cannot close article, cannot find 'X' link.",
+                5
+        );
+
+
+        waitForElementAndClick(
+                By.xpath("//android.widget.FrameLayout[@content-desc= 'My lists']"),
+                "Cannot find navigation button to My list",
+                5
+        );
+
+        waitForElementAndClick(
+                By.xpath("//*[@resource-id = 'org.wikipedia:id/item_container']//*[@text= '" + name_of_folder + "']"),
+                "Cannot find creation folder'Learning programming'",
+                5
+        );
+
+        swipeElementToLeft(
+                By.xpath("//*[@text= 'Java (programming language)']"),
+                "Cannot find saved article 'Java (programming language)'"
+        );
+
+        waitForElementNotPresent(
+                By.xpath("//*[@text= 'Java (programming language)']"),
+                "Cannot delete  saved article ",
+                5
+        );
+
+        waitForElementPresent(
+                By.xpath("//*[@resource-id = 'org.wikipedia:id/page_list_item_container']//*[@text='Python (programming language)']"),
+                "Cannot find saved article 'Python' in save folder",
+                5
+
         );
     }
 
